f639825a10690d5922448b6d4586d11dc219a619
Sumnicky99 <sumnicky99@gmail.com>
2024-05-21 11:50:44 +0300

    done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,37 @@
 
 def happy_new_year():
     # code goes here!
-    pass
-
+    i=10
+    while i>0 :
+        print(i)
+        i -= 1
+        print("Happy New Year!")
+        
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # code goes here!                                       
+       squared_list = [x ** 2 for x in int_list]
+       return squared_list
+
+# trying it out
+input_list = [1, 2, 3, 4, 5]
+result = square_integers(input_list)
+print(result)  
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+      # Iterate from 1 to 100
+    for i in range(1, 101):
+        # Check if the number is divisible by both 3 and 5 first, as it's the most specific condition
+        #if you divide this number(i) is the reminder equal to zeroo?? if yess print
+        if i % 3 == 0 and i % 5 == 0: 
+            print("FizzBuzz")
+        # Check if the number is divisible by 3
+        elif i % 3 == 0:
+            print("Fizz")
+        # Check if the number is divisible by 5
+        elif i % 5 == 0:
+            print("Buzz")
+        # If none of the above conditions are met, simply print the number
+        else:
+            print(i)
